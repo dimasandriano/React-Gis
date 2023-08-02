3ee0cae85daf1fd5434c5aff814270c26d3bcab3
Fahmi Ahsan <fahmiahsan00@gmail.com>
2023-08-02 19:57:04 +0700

    commit

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,49 +5,45 @@ import { Link } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 function Dashboard() {
-	const setMahasiswa = useStore((state) => state.setMahasiswa);
-	const mahasiswa = useStore((state) => state.mahasiswa);
-	const deleteMahasiswa = useStore((state) => state.deleteMahasiswa);
-	const logout = useStore((state) => state.logout);
-	useEffect(() => {
-		setMahasiswa();
-	}, []);
-	const handleDelete = async (id) => {
-		try {
-			await deleteMahasiswa(id);
-			toast.success("Berhasil menghapus data mahasiswa");
-		} catch (error) {
-			toast.error("Gagal menghapus data mahasiswa");
-		}
-	};
-	return (
-		<div>
-			<div className="mx-auto w-full px-6 lg:max-w-5xl xl:max-w-7xl">
-				<div className="flex justify-between">
-					<Link to="/create-mahasiswa">
-						<button className="py-2 px-4 text-white bg-emerald-500 rounded-lg mb-3">
-							Tambah
-						</button>
-					</Link>
-					<div className="flex gap-2">
-						<Link to="/">
-							<button className="py-2 px-4 text-white bg-emerald-500 rounded-lg mb-3">
-								Home
-							</button>
-						</Link>
-						<button
-							onClick={logout}
-							className="py-2 px-4 text-white bg-red-500 rounded-lg mb-3">
-							Logout
-						</button>
-					</div>
-				</div>
-				<div className="w-full overflow-x-auto">
-					<Table mahasiswa={mahasiswa} handleDelete={handleDelete} />
-				</div>
-			</div>
-		</div>
-	);
+  const setMahasiswa = useStore((state) => state.setMahasiswa);
+  const mahasiswa = useStore((state) => state.mahasiswa);
+  const deleteMahasiswa = useStore((state) => state.deleteMahasiswa);
+  const logout = useStore((state) => state.logout);
+  useEffect(() => {
+    setMahasiswa();
+  }, []);
+  const handleDelete = async (id) => {
+    try {
+      await deleteMahasiswa(id);
+      toast.success("Berhasil menghapus data mahasiswa");
+    } catch (error) {
+      toast.error("Gagal menghapus data mahasiswa");
+    }
+  };
+  return (
+    <div>
+      <div className="bg-violet-200 mb-8">
+        <div className="mx-auto w-full px-6 lg:max-w-5xl xl:max-w-7xl">
+          <div className="flex justify-between">
+            <Link to="/create-mahasiswa">
+              <button className="py-2 px-4 text-white bg-emerald-500 rounded-lg mb-3">Tambah</button>
+            </Link>
+            <div className="flex gap-2">
+              <Link to="/">
+                <button className="py-2 px-4 text-white bg-emerald-500 rounded-lg mb-3">Home</button>
+              </Link>
+              <button onClick={logout} className="py-2 px-4 text-white bg-red-500 rounded-lg mb-3">
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full overflow-x-auto">
+        <Table mahasiswa={mahasiswa} handleDelete={handleDelete} />
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
